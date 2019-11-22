43b4a565ad34473dcfacaa73cd33de1971327fcd
unknown <fabian.rhcp@gmail.com>
2019-11-22 17:25:47 -0600

    Testing find stream

---
+++ report_building_files/csv_converter_before_crash_fix.js
@@ -0,0 +1,57 @@
+const { parseAsync } = require("json2csv");
+const nodemailer = require("nodemailer");
+
+const convertToCSVandEmail = async (email_recipients, email_body, data, attachment_extension) => {
+
+  const fields = ["sensor_name", "output_data", "reading_date"];
+  const opts = { fields };
+
+  parseAsync(data, opts)
+    .then(csv => {
+
+      async function main() {
+        // create reusable transporter object using the default SMTP transport
+        let transporter = nodemailer.createTransport({
+          host: "version01.com",
+          port: 465,
+          secure: true,
+          auth: {
+            user: "report_delivery@version01.com",
+            pass: "YnZvD41ERVx_"
+          }
+        });
+    
+        // send mail with defined transport object
+        let info = await transporter.sendMail({
+          from: '"Bullseye Report Delivery" <report_delivery@version01.com>',
+          to: email_recipients,
+          subject: "Bullseye Production report",
+          html: email_body,
+          attachments: [
+            {
+              filename: "report." + attachment_extension,
+              content: Buffer.from(csv, "utf-8")
+            }
+          ]
+        });
+    
+        console.log("Message sent: %s", info.messageId);
+        // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+    
+        // Preview only available when sending through an Ethereal account
+        // console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+        // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...    
+      }
+      main().catch(console.error);
+
+    })
+    .catch(err => console.error(err));
+};
+
+module.exports = convertToCSVandEmail;
+
+
+
+
+
+
--- routes/reports_router_before_streams.js
+++ routes/reports_router_before_streams.js
@@ -59,7 +59,6 @@ router.post("/schedule-report", authMiddleware, async (req, res) => {
     object_of_jobs[saved_preliminar_report.report_internal_name] = new CronJob( new Date("" + req.body.scheduling_date),
       async function() {
         try {
-          // let sensor_readings_array_for_report = await Sensor.find(
           let sensor_readings_array_for_report = await Sensor.find(
             {
               // Criteria to find the document
@@ -78,29 +77,16 @@ router.post("/schedule-report", authMiddleware, async (req, res) => {
               // Order of the records
               sort: { reading_date: -1 } // Sorting by the newest usign reading date as criteria
             }
-          ).cursor();
-          //)/*.limit(200);*/
-
-          
-
-          sensor_readings_array_for_report.on("data", (doc) =>{
-            let formatted_document_date = new Date(+doc.reading_date);            
-            doc.reading_date = formatted_document_date.toLocaleDateString() + " " + formatted_document_date.toLocaleTimeString("es-MX");
-            console.log(doc);
-          });
-
-          sensor_readings_array_for_report.on("close", ()=> {
-            console.log("Done");
-          });
+          )/*.limit(200);*/
 
           // Convertindg date milliseconds to string date before sending the report
 
-          // sensor_readings_array_for_report.forEach( (element) => {
-          //   let formatted_document_date = new Date(+element.reading_date);
-          //   element.reading_date = formatted_document_date.toLocaleDateString() + " " + formatted_document_date.toLocaleTimeString("es-MX");
-          // });
+          sensor_readings_array_for_report.forEach( (element) => {
+            let formatted_document_date = new Date(+element.reading_date);
+            element.reading_date = formatted_document_date.toLocaleDateString() + " " + formatted_document_date.toLocaleTimeString("es-MX");
+          });
     
-          //await convertToCSVandEmail(saved_report.report_distribution_list, saved_report.report_email_body, sensor_readings_array_for_report, "csv" );
+          await convertToCSVandEmail(saved_report.report_distribution_list, saved_report.report_email_body, sensor_readings_array_for_report, "csv" );
           
           saved_report.status = "sent";
           await saved_report.save();
