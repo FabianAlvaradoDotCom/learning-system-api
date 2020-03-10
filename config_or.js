5838a18c1ee9291e2f8e520a0b5a47a03670445a
Luis Fabian Alvarado Ramirez <luisfabianalvaradoramirez@Itexicos-MacBook-Pro.local>
2020-03-10 10:49:09 -0600

    synching

---
+++ config_or.js
@@ -0,0 +1,8 @@
+module.exports = {
+  dbURL:
+    "mongodb://learning-system-super-admin:leaSysAd01@ds061751.mlab.com:61751/learning-system-app",
+  dbUser: "learning-system-super-admin",
+  dbPass: "leaSysAd01",
+  secret: "vHykHVMwUs6j"
+};
+//ggg
